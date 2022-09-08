6d911883ffe7f49ec538aa2cc40e301ed283bd2e
Ivan <krew701@gmail.com>
2022-09-08 13:34:13 +0300

    some changes

---
+++ components/slider.php
@@ -0,0 +1,31 @@
+<div class="carousel carousel__choice">
+    <div class="carousel__inner">
+        <div class="choice__item">
+            <div class="item__img">
+                <img src="img/choice/smart-terminal.svg" alt="smart-terminal">
+            </div>
+            <div class="item__footer">
+                <div class="item__footer-title">Смарт-терминал</div>
+                <div class="item__footer-descr">Умеет всё: карты, чеки, оплату по <br> СБП</div>
+            </div>
+        </div>
+        <div class="choice__item">
+            <div class="item__img">
+                <img src="img/choice/mobile-terminal.svg" alt="mobile-terminal">
+            </div>
+            <div class="item__footer">
+                <div class="item__footer-title">Мобильный терминал</div>
+                <div class="item__footer-descr">Работает с любым гаджетом по <br> Bleutooth</div>
+            </div>
+        </div>
+        <div class="choice__item">
+            <div class="item__img">
+                <img src="img/choice/delivery-smartphone.svg" alt="delivery-smartphone">
+            </div>
+            <div class="item__footer">
+                <div class="item__footer-title">Смартфон курьера</div>
+                <div class="item__footer-descr">Терминал в телефоне: работает на Android, принимает карты и оплату через СБП</div>
+            </div>
+        </div>
+    </div>
+</div>
